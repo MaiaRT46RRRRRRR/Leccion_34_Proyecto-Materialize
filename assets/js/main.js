3d4693f2d932f819d4178c5ccce23baa53bbbb23
GreeceArtemis <greece.rapu@gmail.com>
2017-05-18 15:33:41 -0500

    arreglando padding

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,19 @@
 
   $('#textarea1').trigger('autoresize');
 window.onload=function(){
-
   var soloLetras = function(e){
   var codigoTecla = e.keyCode;
-  if((codigoTecla>=97 && codigoTecla<=122) || (codigoTecla>=65 && codigoTecla<=90)|| codigoTecla==39 || codigoTecla == 32){
-    this.classList.add("valid");
-    this.classList.remove("invalid");
-    return true;
-  }else{
-    this.classList.remove("valid");
-    this.classList.add("invalid");
-    return false;
+  if((codigoTecla>=97 && codigoTecla<=122) || (codigoTecla>=65 && codigoTecla<=90)|| codigoTecla==39 || codigoTecla == 32)
+  {
+      this.classList.add("valid");
+      this.classList.remove("invalid");
+      return true;
+  }
+  else
+  {
+      this.classList.remove("valid");
+      this.classList.add("invalid");
+      return false;
   }
 }
 var name=document.getElementById("first_name");
